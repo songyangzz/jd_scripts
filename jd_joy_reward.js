a12c6ba3a56db1595865d0be2ee473445da3bbb8
songyangzz <623984375@qq.com>
2021-06-24 16:09:54 +0800

    Update jd_joy_reward.js

--- jd_joy_reward.js
+++ jd_joy_reward.js
@@ -211,7 +211,7 @@ async function joyReward() {
 }
 function getExchangeRewards() {
   let opt = {
-    url: "//jdjoy.jd.com/common/gift/getBeanConfigs?reqSource=h5&invokeKey=Oex5GmEuqGep1WLC",
+    url: "//jdjoy.jd.com/common/gift/getBeanConfigs?reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE",
     method: "GET",
     data: {},
     credentials: "include",
@@ -255,7 +255,7 @@ function getExchangeRewards() {
 function exchange(saleInfoId, orderSource) {
   let body = {"buyParam":{"orderSource":orderSource,"saleInfoId":saleInfoId},"deviceInfo":{}}
   let opt = {
-    "url": "//jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=Oex5GmEuqGep1WLC",
+    "url": "//jdjoy.jd.com/common/gift/new/exchange?reqSource=h5&invokeKey=NRp8OPxZMFXmGkaE",
     "data":body,
     "credentials":"include","method":"POST","header":{"content-type":"application/json"}
   }
